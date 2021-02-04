0a008a492dd34a3a68564be9ebcbafe7db983642
mthaissouza <maria.tha2002@gmail.com>
2021-02-04 02:03:00 -0300

    Main view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,37 @@
 package application;
 
-public class Main {
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//instanciação do objeto .fxml
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			
+			Parent parent = loader.load();
+			//cena 
+			Scene mainScene = new Scene(parent);
+			
+			//palco da cena
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Livraria");
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 }
+
+
